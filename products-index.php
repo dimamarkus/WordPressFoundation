0460c872b660d3a3632cdd1dcf2a1bfef22f570d
Dima Markus <dmi3000@gmail.com>
2013-11-05 21:00:14 -0500

    Update product index template
    - Add test courses. (hard coded in)
    
    Signed-off-by: Dima Markus <dmi3000@gmail.com>

--- products-index.php
+++ products-index.php
@@ -22,36 +22,53 @@ get_header(); ?>
  
         <div class="large-4 small-12 columns">
  
-          <img src="http://placehold.it/500x500&text=Logo">
+          <img src="http://placehold.it/500x500&text=Learn Online!">
  
           <div class="hide-for-small panel">
-            <h3>Header</h3>
-            <h5 class="subheader">Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis.
+            <h3>Online Courses</h3>
+            <h5 class="subheader">Check out our variety of online courses.
             </h5>
           </div>
  
-          <a href="#">
-          <div class="panel callout radius">
-            <h6>99&nbsp; items in your cart</h6>
-          </div>
+          <a href="http://shoptest.dubspot.com/checkout" class="button radius">Checkout
+
           </a>
  
         </div>
  
     <!-- End Side Bar -->
  
- 
+
     <!-- Thumbnails -->
  
         <div class="large-8 columns">
           <div class="row">
  
             <div class="large-4 small-6 columns">
-              <img src="http://placehold.it/1000x1000&text=Thumbnail">
+              <img src="http://placehold.it/1000x1000&text=One Cent Course">
  
               <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
+                <h5>The One Cent Course</h5>
+                <h6 class="subheader">$0.01</h6>
+                 <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="392337040" />
+                  <input type="hidden" name="return_to" value="back" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
+              </div>
+            </div>
+ 
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=ONE CENT!!">
+ 
+              <div class="panel">
+                <h5>Another One Cent Course</h5>
+                <h6 class="subheader">$0.01</h6>
+                <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="393570220" />
+                  <input type="hidden" name="return_to" value="back" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
               </div>
             </div>
  
@@ -59,77 +76,70 @@ get_header(); ?>
               <img src="http://placehold.it/500x500&text=Thumbnail">
  
               <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
+                <h5>Music Foundations</h5>
+                <h6 class="subheader">$1795.50</h6>
+                <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="378547371" />
+                  <input type="hidden" name="return_to" value="back" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
               </div>
             </div>
+
+            <div class="large-4 small-6 columns">
+              <img src="http://placehold.it/500x500&text=LOGIC">
  
+              <div class="panel">
+                <h5>Logic Pro X<h5>
+                <h6 class="subheader">$4999.99</h6>
+                <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="389347028" />
+                  <input type="hidden" name="return_to" value="http://shoptest.dubspot.com/checkout" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
+              </div>
+            </div>
+
             <div class="large-4 small-6 columns">
               <img src="http://placehold.it/500x500&text=Thumbnail">
  
               <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
+                <h5>Test Course</h5>
+                <h6 class="subheader">$999.50</h6>
+                <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="378547371" />
+                  <input type="hidden" name="return_to" value="back" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
               </div>
             </div>
- 
+
             <div class="large-4 small-6 columns">
               <img src="http://placehold.it/500x500&text=Thumbnail">
  
               <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
+                <h5>Test Course</h5>
+                <h6 class="subheader">$999.50</h6>
+                <form action="http://shoptest.dubspot.com/cart/add" method="post">
+                  <input type="hidden" name="id" value="378547371" />
+                  <input type="hidden" name="return_to" value="back" />
+                  <input type="submit" value="Add To Cart" class="small rounded button"/>
+                </form>
               </div>
             </div>
- 
-            <div class="large-4 small-6 columns">
-              <img src="http://placehold.it/500x500&text=Thumbnail">
- 
-              <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
-              </div>
-            </div>
- 
-            <div class="large-4 small-6 columns">
-              <img src="http://placehold.it/500x500&text=Thumbnail">
- 
-              <div class="panel">
-                <h5>Item Name</h5>
-                <h6 class="subheader">$000.00</h6>
-              </div>
-            </div>
-          </div>
+
+         </div>
+
+
  
     <!-- End Thumbnails -->
  
  
-    <!-- Managed By -->
-          <div class="row">
-            <div class="large-12 columns">
-              <div class="panel">
-                <div class="row">
- 
-                  <div class="large-2 small-6 columns">
-                    <img src="http://placehold.it/300x300&text=Site Owner">
-                  </div>
- 
-                  <div class="large-10 small-6 columns">
-                    <strong>This Site Is Managed By<hr/></strong>
- 
-                    Risus ligula, aliquam nec fermentum vitae, sollicitudin eget urna. Donec dignissim nibh fermentum odio ornare sagittis
-                  </div>
- 
-                </div>
-              </div>
-            </div>
- 
-    <!-- End Managed By -->
  
           </div>
         </div>
       </div>
  
 
-		
+    
 <?php get_footer(); ?>
